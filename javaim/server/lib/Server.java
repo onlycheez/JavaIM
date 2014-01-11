bb8130e0863f015e1ec1743718928e7bb06df1ad
Jan Sidlo <only.cheez@gmail.com>
2014-01-11 16:58:09 +0100

    Updated indentation in server according to Android code style
    Forgotten file.
    See http://source.android.com/source/code-style.html#use-spaces-for-indentation
    One difference - lines doesn't exceed 80 columns.

--- javaim/server/lib/Server.java
+++ javaim/server/lib/Server.java
@@ -14,47 +14,47 @@ import java.io.*;
 public class Server {
 
   private final static int PORT = 4444;
-  private ServerSocket serverSocket = null;
-  private ArrayList<Thread> clients = null;
-  private ArrayList<Worker> workers = null;
+    private ServerSocket serverSocket = null;
+    private ArrayList<Thread> clients = null;
+    private ArrayList<Worker> workers = null;
 
-  public Server() throws IOException {
-    serverSocket = new ServerSocket(PORT);
-    clients = new ArrayList<>();
-    workers = new ArrayList<>();
-  }
-
-  /**
-   * Opens TCP socket and waits for client connections.
-   */
-  public void run() throws IOException {
-    System.out.println("Server is running");
-
-    while (true) {
-      Socket clientSocket = serverSocket.accept();
-      Worker worker = new Worker(this, clientSocket);
-      workers.add(worker);
-      Thread client = new Thread(worker);
-      client.start();
-      clients.add(client);
-    }
-  }
-
-  public void passMessage(String from, String to, String message) {
-    boolean found = false;
-
-    for (Worker worker : workers) {
-      if (worker.getIsLogged() && worker.getUsername().equals(to)) {
-        worker.sendMessage(from, to, message);
-        found = true;
-        break;
-      }
+    public Server() throws IOException {
+        serverSocket = new ServerSocket(PORT);
+        clients = new ArrayList<>();
+        workers = new ArrayList<>();
     }
 
-    if (!found) {
-      System.out.println("Username " + to + " was not found.");
+    /**
+     * Opens TCP socket and waits for client connections.
+     */
+    public void run() throws IOException {
+        System.out.println("Server is running");
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Worker worker = new Worker(this, clientSocket);
+            workers.add(worker);
+            Thread client = new Thread(worker);
+            client.start();
+            clients.add(client);
+        }
     }
-  }
+
+    public void passMessage(String from, String to, String message) {
+        boolean found = false;
+
+        for (Worker worker : workers) {
+            if (worker.getIsLogged() && worker.getUsername().equals(to)) {
+                worker.sendMessage(from, to, message);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Username " + to + " was not found.");
+        }
+    }
 
     synchronized public void clientDisconnected(String username) {
         for (int i = 0; i < workers.size(); i++) {
